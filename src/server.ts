ab1a1ed7a3e81e04eab2d71afa09acdbc952e420
Marcello C <marcello_ct@hotmail.com>
2026-09-10 21:46:25 -0300

    add errorHandling to app.use()

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { errorHandling } from './middlewares/errorHandling.js';
 import { routes } from './routes/index.routes.js';
 const PORT = 3333;
 
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errorHandling);
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
