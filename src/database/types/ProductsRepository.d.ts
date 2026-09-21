8acf0a00c9a2f86d816a42f9f906328d7562dfa9
Marcello C <marcello_ct@hotmail.com>
2026-09-21 18:11:45 -0300

    products type

---
+++ src/database/types/ProductsRepository.d.ts
@@ -0,0 +1,10 @@
+type ProductsRepository = {
+  product_id: number;
+  title: string;
+  image: string;
+  price: number;
+  description: string;
+  stock: number;
+  created_at: number;
+  updated_at: number;
+};
